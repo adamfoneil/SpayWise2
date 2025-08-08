2e7c90ce2f77ed74f53bd100a0201d2ca9e1f49f
Adam O'Neil <adamosoftware@gmail.com>
2025-08-08 08:22:26 -0400

    auth attributes and stubbed out clients pages

---
+++ HydroApp/Pages/Clients/Owners.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HydroApp.Pages.Clients
+{
+    public class OwnersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
