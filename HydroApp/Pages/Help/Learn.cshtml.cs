0f5dd85af14bdc7367448e6407006a0d5e5480f1
Adam O'Neil <adamosoftware@gmail.com>
2025-08-14 21:31:49 -0400

    more core page stubs

---
+++ HydroApp/Pages/Help/Learn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HydroApp.Pages.Help
+{
+    public class LearnModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
