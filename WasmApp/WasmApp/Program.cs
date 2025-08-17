aae5ba3b96f334c0a1a825a4b477ae1327d98226
Adam O'Neil <adamosoftware@gmail.com>
2025-08-17 06:54:40 -0400

    added wasm app

---
+++ WasmApp/WasmApp/Program.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SpayWise.Data;
+using WasmApp;
+using WasmApp.Components;
+using WasmApp.Components.Account;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var logLevels = new ApplicationLogLevels();
+var connectionString = AppDbFactory.GetConnectionString(builder.Configuration, args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+	.AddInteractiveWebAssemblyComponents()
+	.AddAuthenticationStateSerialization();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+
+builder.Services.AddAuthentication(options =>
+	{
+		options.DefaultScheme = IdentityConstants.ApplicationScheme;
+		options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+	})
+	.AddIdentityCookies();
+builder.Services.AddAuthorization();
+
+
+builder.Services.AddDbContextFactory<SpayWiseDbContext>(options =>
+	options.UseNpgsql(connectionString).EnableDetailedErrors().EnableSensitiveDataLogging());
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+	.AddEntityFrameworkStores<SpayWiseDbContext>()
+	.AddSignInManager()
+	.AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseWebAssemblyDebugging();
+	app.UseMigrationsEndPoint();
+}
+else
+{
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+	.AddInteractiveWebAssemblyRenderMode()
+	.AddAdditionalAssemblies(typeof(WasmApp.Client._Imports).Assembly);
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+app.Run();
