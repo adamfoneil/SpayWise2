e461aa93425594e15cfd2669ef12ee39ecf661f3
Adam O'Neil <adamosoftware@gmail.com>
2025-08-17 06:54:53 -0400

    refactors in progress

---
+++ HydroApp/GridHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpayWise.Data;
+
+namespace HydroApp;
+
+public abstract class GridHelper<TEntity>(
+	CurrentUserService currentUser,
+	IDbContextFactory<SpayWiseDbContext> dbFactory) where TEntity : class
+{
+	protected readonly CurrentUserService CurrentUser = currentUser;
+	protected readonly IDbContextFactory<SpayWiseDbContext> DbFactory = dbFactory;
+
+	protected abstract IQueryable<TEntity> GetQuery(SpayWiseDbContext dbContext);
+
+	public async Task<TEntity[]> QueryAsync()
+	{
+		var (appUser, clinicUser) = await CurrentUser.GetAsync();
+		
+		using var dbContext = DbFactory.CreateDbContext();
+		var query = GetQuery(dbContext).Where(row => EF.Property<int>(row, "ClinicId") == clinicUser!.ClinicId);
+		return await query.AsNoTracking().ToArrayAsync();
+	}
+}
